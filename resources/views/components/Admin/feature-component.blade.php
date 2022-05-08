67d3966c9423f2066527a843356ec03a8a9b48c6
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-05-08 10:14:55 +0200

    fix feature banner

--- resources/views/components/Admin/feature-component.blade.php
+++ resources/views/components/Admin/feature-component.blade.php
@@ -10,11 +10,11 @@
 
         <div class="d-flex mb-2">
             <div class="group w-100">
-                <input placeholder="Header Text In English" class="form-control" name="header_text_in_english">
+                <input placeholder="Header Text In English" class="form-control" name="header_text_in_english" value="{{$banner->header_text_in_english}}">
                 <p class="error error_header_text_in_english"></p>
             </div>
             <div class="group w-100">
-                <input placeholder="Header Text In French" class="form-control mx-2" name="header_text_in_french">
+                <input placeholder="Header Text In French" class="form-control mx-2" name="header_text_in_french" value="{{$banner->header_text_in_french}}">
                 <p class="error error_header_text_in_french"></p>
             </div>
         </div>
@@ -25,11 +25,11 @@
 
         <div class="d-flex mb-2">
             <div class="group w-100">
-                <input placeholder="Upper Text In English" class="form-control" name="upper_text_in_english">
+                <input placeholder="Upper Text In English" class="form-control" name="upper_text_in_english" value="{{$banner->upper_text_in_english}}">
                 <p class="error error_upper_text_in_english"></p>
             </div>
             <div class="group w-100">
-                <input placeholder="Upper Text In French" class="form-control mx-2" name="upper_text_in_french">
+                <input placeholder="Upper Text In French" class="form-control mx-2" name="upper_text_in_french" value="{{$banner->upper_text_in_french}}">
                 <p class="error error_upper_text_in_french"></p>
             </div>
         </div>
@@ -40,11 +40,11 @@
 
         <div class="d-flex mb-2">
             <div class="group w-100">
-                <input placeholder="Button Text English" class="form-control" name="button_text_in_english">
+                <input placeholder="Button Text English" class="form-control" name="button_text_in_english" value="{{$banner->button_text_in_english}}">
                 <p class="error error_button_text_in_english"></p>
             </div>
             <div class="group w-100">
-                <input placeholder="Button Text In French" class="form-control mx-2" name="button_text_in_french">
+                <input placeholder="Button Text In French" class="form-control mx-2" name="button_text_in_french" value="{{$banner->button_text_in_french}}">
                 <p class="error error_button_text_in_french"></p>
             </div>
         </div>
@@ -54,24 +54,25 @@
         </label>
 
         <div class="group">
-            <input placeholder="Redirect To" class="form-control mb-2" name="redirect">
+            <input placeholder="Redirect To" class="form-control mb-2" name="redirect" value="{{$banner->redirect}}">
             <p class="error error_redirect"></p>
         </div>
 
         <div class="row mb-4">
             <div class="col-sm-12 col-md-4 mg-t-10 mg-sm-t-0">
-                <input type="file" class="dropify" data-default-file="" data-height="200"  name="background"/>
+                <input type="file" class="dropify" data-default-file="@if($banner->background) {{ asset('storage/banner/large/'.$banner->background->name) }} @endif" data-height="200"  name="background"/>
             </div>
             <div class="mx-2 col-md-6">
                 <label for="Tumbnail">Background</label>
                 <p class="mt-2 sub-text">
-                    Enter Beautiful background to the tour and please add image in aspect ratio to get the best performance
+                    Enter Beautiful background to the banner and please add image in aspect ratio to get the best performance
+                    (Note: optimum size: 1679x490)
                 </p>
                 <p class="error error_background"></p>
             </div>
         </div>
 
-        <input type="hidden" value="feature_input" name="feature_input">
+        <input type="hidden" name="type" value="feature">
 
         <button class="btn-primary">Submit</button>
     </div>
