3a444061c23c7386f529409f61c388336add9fc3
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-05-11 09:55:39 +0200

    TourTest

---
+++ tests/Feature/TourTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+
+class TourTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+     /** @test  */
+    public function a_tour_can_be_store()
+    {
+        $this->withExceptionHandling();
+
+        $thumbnail = UploadedFile::fake()->image('thumbnail.jpg');
+        $user = User::find(1);
+        
+        $response = $this->actingAs($user)->post('admin-panal/blog/store',[
+            'thumbnail' => $thumbnail,
+            'article_in_fr' => 'article_in_fr',
+            'article_in_en' => 'article_in_en',
+            'language' => 'mixed',
+        ]);
+
+        $response->assertOk();
+    }
+}
