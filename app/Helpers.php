c97dd745bde33ebe023c0c0ab503f33b717d63a4
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-05-12 13:54:32 +0200

    currency helpers

--- app/Helpers.php
+++ app/Helpers.php
@@ -12,3 +12,28 @@ function currency_sympol()
         return '$';
     }
 }
+
+function currency_value($package)
+{
+    if ( app()->make('saved_cookie',['type' => 'currency']) == 'cad' ) {
+        return $package->cad_price ?? 'Not Set';
+    } else if ( app()->make('saved_cookie',['type' => 'currency']) == 'eur' ) {
+        return $package->eur_price ?? 'Not Set';
+    } else {
+        return $package->usd_price;
+    }
+}
+
+function get_local_string($string)
+{
+    return str_replace(' ','_',strtolower($string));
+}
+
+function get_local($english,$french)
+{
+    if(Config::get('app.locale') == 'en') {
+        return $english;
+    } else {
+        return $french;
+    }
+}
