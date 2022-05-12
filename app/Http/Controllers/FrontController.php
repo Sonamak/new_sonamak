47d31b6a2b2943619199d8ae0150bee23b3065be
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-05-12 13:55:59 +0200

    tour details route

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tour;
 use Illuminate\Http\Request;
-
+use Share;
 class FrontController extends Controller
 {
     public function home()
     {
         return view('front.home');
     }
+
+    public function tour(Tour $tour)
+    {
+        return view('front.tour.index',[
+            'tour' => $tour
+        ]);
+    }
+
+    public function share($provider)
+    {
+        if ( $provider == 'facebook' ) 
+            return redirect()->away(Share::currentPage()->facebook()->getRawLinks());
+        else if ( $provider == 'twitter' )
+            return redirect()->away(Share::currentPage()->twitter()->getRawLinks());
+
+    }
+
 }
