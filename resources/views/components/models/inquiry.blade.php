8077ef81ec3054e4cca9c6ae5fd6c90c7a0c31fa
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-05-22 16:27:57 +0200

    localized inquiry

--- resources/views/components/models/inquiry.blade.php
+++ resources/views/components/models/inquiry.blade.php
@@ -11,7 +11,7 @@
             </div>
             <div class="col-md-6 p-3">
                 <h2 class="modal_text">
-                    Send your inquires to us
+                   {{__('main.send_your_inquires_to_us')}}
                 </h2>
                 <form class="ajax-form">
                    
@@ -20,42 +20,42 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label for="name" class="d-block">Enter your name</label>
+                                <label for="name" class="d-block">{{__('main.enter_your_name')}}</label>
                                 <input type="text" class="form-control" id="name" placeholder="Ex: John Adam" name="name">
                                 <p class="error error_name"></p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="email" class="d-block">Enter your email</label>
+                                <label for="email" class="d-block">{{__('main.enter_your_email')}}</label>
                                 <input type="text" class="form-control" id="email" name="email" placeholder="Ex: JohnAdam@gmail.com">
                                 <p class="error error_email"></p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="telephone" class="d-block">Enter your telephone</label>
+                                <label for="telephone" class="d-block">{{__('main.enter_your_telephone')}}</label>
                                 <input type="text" class="form-control" name="telephone" id="telephone" placeholder="Ex: 0123456789">
                                 <p class="error error_telephone"></p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="department" class="d-block">Enter department date</label>
+                                <label for="department" class="d-block">{{__('main.enter_department_date')}}</label>
                                 <input type="datetime-local" class="form-control" id="department" name="department" placeholder="Ex: 0123456789">
                                 <p class="error error_department"></p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="return" class="d-block">Enter return date</label>
+                                <label for="return" class="d-block">{{__('main.enter_return_date')}}</label>
                                 <input type="datetime-local" class="form-control" id="return" name="return" placeholder="Ex: 0123456789">
                                 <p class="error error_return"></p>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label for="language" class="d-block">Enter tour language</label>
+                                <label for="language" class="d-block">{{__('main.enter_tour_language')}}</label>
                                 <div class="row ml-2">
                                     <p class="col-md-6">
                                         <input type="radio" id="test1"  class="form-check-input" name="language" selected>
@@ -71,18 +71,18 @@
                         </div>
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label for="email" class="d-block">Enter your nationality</label>
+                                <label for="email" class="d-block">{{__('main.enter_your_nationality')}}</label>
                                 @include('components.models.country')
                             </div>
                         </div>
                         <div class="col-md-12">
-                            <label>Enter your inquery</label>
+                            <label>{{__('main.enter_your_inquery')}}</label>
                             <textarea class="form-control" rows="5" name="inquiry"></textarea>
                             <p class="error error_inquiry"></p>
                         </div>
                     </div>
                     <button class="btn btn-primary">
-                        Submit
+                        {{ __('main.submit') }}
                     </button>
                 </form> 
             </div>
