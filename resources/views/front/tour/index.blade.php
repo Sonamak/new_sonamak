95d9a482a0954af553d168b9f4e84d85b32a9133
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-05-22 14:36:32 +0200

    add aside to include

--- resources/views/front/tour/index.blade.php
+++ resources/views/front/tour/index.blade.php
@@ -52,48 +52,7 @@
 					</div>
 					<!-- /col -->
 					
-					<aside class="col-lg-4" id="sidebar">
-						<div class="box_detail booking">
-							<div class="form-group input-dates">
-								<input class="form-control" type="text" name="dates" placeholder="When..">
-								<i class="icon_calendar"></i>
-							</div>
-							<div class="panel-dropdown">
-								<a href="#">Guests <span class="qtyTotal">1</span></a>
-								<div class="panel-dropdown-content right">
-									<div class="qtyButtons">
-										<label>Adults</label>
-										<input type="text" name="qtyInput" value="1">
-									</div>
-									<div class="qtyButtons">
-										<label>Childrens</label>
-										<input type="text" name="qtyInput" value="0">
-									</div>
-								</div>
-							</div>
-							<div class="form-group">
-								<input class="form-control" type="text" name="name" placeholder="Full Name">
-							</div>
-							<div class="form-group">
-								<input class="form-control" type="email" name="email" placeholder="email">
-							</div>
-							<div class="form-group">
-								<input class="form-control" type="text" name="nationality" placeholder="Nationality">
-							</div>
-							<div class="form-group">
-								<input class="form-control" type="text" name="phone" placeholder="Telephone">
-							</div>
-							<div class="form-group">
-								<textarea class="form-control" name="requirments" placeholder="Please advise your tour requirements" ></textarea>
-							</div>																																										
-							<a href="cart-1.html" class="btn_1 full-width purchase">Submit Inquiry</a>
-							<div class="text-center"><small>No money charged in this step</small></div>
-						</div>
-						<ul class="share-buttons">
-							<li><a class="fb-share" href="{{ route('share',['provider' => 'facebook']) }}" target="__blank"><i class="social_facebook"></i> Share</a></li>
-							<li><a class="twitter-share" href="{{ route('share',['provider' => 'twitter']) }}" target="__blank"><i class="social_twitter"></i> Tweet</a></li>
-						</ul>
-					</aside>
+					@include('front.tour.aside')
 				</div>
 				<!-- /row -->
 			</div>
