266710c5244ef4634192c031e6e440cfd9cff230
Abdalrhman Hussin <abdalrhmanhussin@gmail.com>
2022-05-22 14:33:06 +0200

    valid date helper

--- app/Helpers.php
+++ app/Helpers.php
@@ -48,3 +48,12 @@ function get_local($english,$french)
         return $french;
     }
 }
+
+function get_valid_date($date) {
+    $date = explode('-',$date);
+    $year = trim($date[2],' ');
+    $month = trim($date[0],' ');
+    $day = trim($date[1],' ');
+    $validDate ="$month/$day/20$year";
+    return date('Y-m-d',strtotime($validDate));
+}   
